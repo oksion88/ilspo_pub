f08c18c719cc3c0a83d957a63f232acda2269aad
oksion88 <admin@ilspo.ru>
2025-03-14 21:10:12 +0800

    upload script

--- install.sh
+++ install.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
-set +e
 
-pacman -Syyu -y --noconfirm &&
+pacman -Syyu -y --noconfirm && || true
 sleep 5 
 rm -f /etc/pacman.conf && 
 rm -f /etc/pacman.d/mirrorlist && 
 cp pacman/pacman.conf /etc/pacman.conf && 
 cp pacman/mirrorlist /etc/pacman.d/mirrorlist &&
 sleep 1
-sudo pacman -Scc -y
-pacman -Syyu -y &&
+sudo pacman -Scc -y || true
+pacman -Syyu -y && || true
 sleep 3
-chmod +x apps/inst.sh
-sh apps/inst.sh
+chmod +x apps/inst.sh 
+sh apps/inst.sh || true
 
 if [[ $(pacman -Syu | grep 'делать больше нечего') ]]; then
         echo "Job Done!"
