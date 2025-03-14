d874ec832b5d550c54fdab7dd203aafde2f43238
oksion88 <admin@ilspo.ru>
2025-03-14 20:11:56 +0800

    upload script

--- install.sh
+++ install.sh
@@ -1,11 +1,13 @@
 # /bin/bash
 
-yes | cp pacman/pacman.conf /etc/pacman.conf
-yes | cp pacman/mirrirlist /etc/pacman.d/mirrorlist
+rm -f /etc/pacman.conf
+rm -f /etc/pacman.d/mirrorlist
+yes | cp ./pacman/pacman.conf /etc/pacman.conf
+yes | cp ./pacman/mirrirlist /etc/pacman.d/mirrorlist
 sleep 1
 pacman -Syu -y &&
 sleep 3
-chmod +x apps/inst.sh
-1 | sh apps/inst.sh
+chmod +x ./apps/inst.sh
+1 | sh ./apps/inst.sh
 
 
