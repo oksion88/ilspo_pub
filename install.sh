37e5f947ba0312ae85025ee30f48b775594ced73
oksion88 <admin@ilspo.ru>
2025-03-14 21:26:46 +0800

    upload script

--- install.sh
+++ install.sh
@@ -4,17 +4,18 @@ echo "#####################################################"
 echo "################ HELLO MY FRIEND! ###################"
 echo "#####################################################"
 echo " "
+cp /etc/pacman.conf /etc/pacman.conf.bac
 cp pacman/pacman.conff /etc/pacman.conf && 
 sleep 3 || true
 pacman -Syyu -y --noconfirm || true
 sleep 5 || true
-rm -f /etc/pacman.conf && 
+# rm -f /etc/pacman.conf && 
 rm -f /etc/pacman.d/mirrorlist && 
 # cp pacman/pacman.conf /etc/pacman.conf && 
 cp pacman/mirrorlist /etc/pacman.d/mirrorlist &&
 echo " "
 echo "#####################################################"
-echo "############# FILES FOR PACMAN UPDATED! #############"
+echo "################ PACMAN IS UPDATED! #################"
 echo "#####################################################"
 echo " "
 sleep 3 || true
