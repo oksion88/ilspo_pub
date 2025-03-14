f1a2155cce694e61203eff93d86bd96b4d442038
oksion88 <admin@ilspo.ru>
2025-03-14 21:10:43 +0800

    upload script

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pacman -Syyu -y --noconfirm && || true
+pacman -Syyu -y --noconfirm || true
 sleep 5 
 rm -f /etc/pacman.conf && 
 rm -f /etc/pacman.d/mirrorlist && 
@@ -8,7 +8,7 @@ cp pacman/pacman.conf /etc/pacman.conf &&
 cp pacman/mirrorlist /etc/pacman.d/mirrorlist &&
 sleep 1
 sudo pacman -Scc -y || true
-pacman -Syyu -y && || true
+pacman -Syyu -y || true
 sleep 3
 chmod +x apps/inst.sh 
 sh apps/inst.sh || true
