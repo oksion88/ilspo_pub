5c679c516d88267b13fd7081e353d68564aefd93
oksion88 <admin@ilspo.ru>
2025-03-14 21:44:42 +0800

    upload script

--- apps/inst.sh
+++ apps/inst.sh
@@ -4,6 +4,6 @@ echo "#####################################################"
 echo "################### INSTALL APPS ####################"
 echo "#####################################################"
 echo " "
-fakeroot pacman -S --noconfirm pcmanfm chromium nm-connection-editor i3lock i3 polybar rofi kitty 7zip acl adwaita-cursors adwaita-icon-theme adwaita-icon-theme-legacy alsa-utils arandr audacity blueman bluez bluez-utils bluez-tools brightnessctl bzip2 chrony cifs-utils cmake curl fakeroot firefox gcc git gnu-free-fonts gnupg gvfs gvfs-nfs gvfs-nfs gzip imagemagick iproute2 make mc nano neofetch neon networkmanager networkmanager-l2tp networkmanager-strongswan nfs-utils ntfs-3g openssh openshot os-prober parted python psmisc slim strongswan sudo tar traceroute unrar unzip vlc wget xorg-xinput xorg-server xorg-font-util zsh -y || true
+sudo pacman -S --noconfirm pcmanfm chromium nm-connection-editor i3lock i3 polybar rofi kitty 7zip acl adwaita-cursors adwaita-icon-theme adwaita-icon-theme-legacy alsa-utils arandr audacity blueman bluez bluez-utils bluez-tools brightnessctl bzip2 chrony cifs-utils cmake curl fakeroot firefox gcc git gnu-free-fonts gnupg gvfs gvfs-nfs gvfs-nfs gzip imagemagick iproute2 make mc nano neofetch neon networkmanager networkmanager-l2tp networkmanager-strongswan nfs-utils ntfs-3g openssh openshot os-prober parted python psmisc slim strongswan sudo tar traceroute unrar unzip vlc wget xorg-xinput xorg-server xorg-font-util zsh -y || true
 echo "################### INSTALL DONE ####################"
 sleep 3 || true
--- install.sh
+++ install.sh
@@ -5,16 +5,16 @@ echo "################ HELLO MY FRIEND! ###################"
 echo "#####################################################"
 echo " "
 sudo pacman -S pacman fakeroot -y --noconfirm || true
-cp /etc/pacman.conf /etc/pacman.conf.bac || true
-cp pacman/pacman.conf /etc/pacman.conf &&
+sudo cp /etc/pacman.conf /etc/pacman.conf.bac || true
+sudo cp pacman/pacman.conf /etc/pacman.conf &&
 #cp pacman/pacman.conff /etc/pacman.conf && 
 sleep 3 || true
-pacman -Syyu -y --noconfirm || true
+sudo pacman -Syyu -y --noconfirm || true
 sleep 5 || true
 # rm -f /etc/pacman.conf && 
-rm -f /etc/pacman.d/mirrorlist && 
+sudo rm -f /etc/pacman.d/mirrorlist && 
 # cp pacman/pacman.conf /etc/pacman.conf && 
-cp pacman/mirrorlist /etc/pacman.d/mirrorlist &&
+sudo cp pacman/mirrorlist /etc/pacman.d/mirrorlist &&
 echo " "
 echo "#####################################################"
 echo "################## PACMAN UPDATED! ##################"
@@ -22,13 +22,13 @@ echo "#####################################################"
 echo " "
 sleep 3 || true
 # pacman -Scc -y || true
-pacman -Syyu -y || true
+sudo pacman -Syyu -y || true
 sleep 3 || true
 chmod +x apps/inst.sh 
 chmod +x apps/config.sh 
 sh apps/inst.sh || true
 sh apps/config.sh || true
 
-if [[ $(pacman -Syu | grep 'делать больше нечего') ]]; then
+if [[ $(sudo pacman -Syu | grep 'делать больше нечего') ]]; then
         echo "Job Done!"
 fi
